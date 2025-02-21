1442073c7d65dbe2e948b97eb9fdf3e29e9686d1
= <=>
2025-02-21 23:42:26 +0530

    origin added

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 3001;
 
 const allowedOrigins = [
   'http://localhost:3001',
-  'http://localhost:3000'
+  'http://localhost:3000',
+  'https://connectinhive.netlify.app'
 ];
 
 const corsOptions = {
